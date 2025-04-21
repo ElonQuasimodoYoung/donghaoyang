c8f4f53674526e2d9e115b37a906b473b5b39796
Elon Young <donghao1@student.unimelb.edu.au>
2025-04-21 15:00:58 +1000

    render the button

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { BtnList } from '@/app/data'
 import React from 'react'
+import NavButton from './NavButton';
 
 const Navigation = () => {
 
@@ -8,7 +9,7 @@ const Navigation = () => {
 
   return (
     <div className='w-full fixed h-screen flex items-center justify-center'>
-      <div className='flex items-center justify-between relative'>
+      <div className='w-max flex items-center justify-between relative'>
       {
         BtnList.map((btn, index) => {
           const angleRad = (index*angleIncrement*Math.PI)/180
@@ -16,9 +17,9 @@ const Navigation = () => {
           const x = `calc(${radius}*${Math.cos(angleRad)})`
           const y = `calc(${radius}*${Math.sin(angleRad)})`
           
-          return <button key={index} className='absolute' style = {{
-            transform: `translate(${x}, ${y})`
-          }}>{btn.label}</button>
+          return <NavButton key={btn.label} x={x} y={y} {...btn} />
+
+
         })
       }
     </div>
